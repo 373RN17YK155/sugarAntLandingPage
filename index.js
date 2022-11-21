ed23eb0a66dcf5804688f2bc3f6673a14c4b0825
373RN17YK155 <vladislav.burko.worker@gmail.com>
2022-11-21 15:50:27 +0300

    CRM-4850: FE.Landing Page
       - rewrite animation

--- index.js
+++ index.js
@@ -14,22 +14,33 @@ collapsePanelList.forEach((element) =>
 
 const convenientWorkflow = document.querySelector('#convenientWorkflow')
 
+document
+  .querySelectorAll('.feature-item')
+  .forEach(
+    (featuresItem) =>
+      (featuresItem.style.top = document.querySelector('.feature__header').getBoundingClientRect().height + 'px')
+  )
+
+const features = document.querySelector('.features')
+const featuresItemsWrapper = document.querySelector('.features__item-wrapper')
+
+const featuresHeaderHeight = document.querySelector('.feature__header').getBoundingClientRect().height
+const featureItemHeight = document.querySelector('.feature-item').getBoundingClientRect().height
+
+featuresItemsWrapper.style.height = featureItemHeight * 2 * document.querySelectorAll('.feature-item').length + 'px'
+featuresItemsWrapper.style.gap = featureItemHeight + 'px'
+
+document.querySelectorAll('.feature-item__img-wrapper').forEach((imageWrapper, index) => {
+  console.log(features.offsetTop + featuresHeaderHeight + index * (featureItemHeight * 2))
+})
+
 document.addEventListener('scroll', () => {
-  const item = document.querySelector('#features')
-
   document.querySelectorAll('.feature-item__img-wrapper').forEach((imageWrapper, index) => {
-    const animStart =
-      item.offsetTop +
-      imageWrapper.clientHeight * (index + 1) -
-      window.innerHeight +
-      index * 300 +
-      index * window.innerHeight
-    const animEnd = item.offsetTop + imageWrapper.clientHeight * (index + 1) + index * 300 + index * window.innerHeight
-
+    const animStart = features.offsetTop + featuresHeaderHeight + index * (featureItemHeight * 2)
+    const animEnd = animStart + featureItemHeight
     const offsetY = Math.max(Math.min((animEnd - window.scrollY) / (animEnd - animStart), 1), -1)
 
-    setParalax(offsetY, Array.from(imageWrapper.children))
-    imageWrapper.parentElement.style.opacity = Math.min(offsetY + 1, 1)
+    setParalax(offsetY, imageWrapper)
   })
 })
 
@@ -38,8 +49,9 @@ function toggleOverflowMenuVisible() {
   main.classList.toggle('main__overlay')
 }
 
-function setParalax(offset, images) {
-  images.forEach((item) => {
+function setParalax(offset, imageWrapper) {
+  imageWrapper.parentElement.style.opacity = Math.max(Math.min(offset + 0.5, 1), 0)
+  Array.from(imageWrapper.children).forEach((item) => {
     item.style.transform = `translate3d(0, ${offset * (window.innerHeight * 0.8)}px, ${
       item.dataset.zIndex * 100
     }px) scale(${-item.dataset.zIndex + 0.5})`
